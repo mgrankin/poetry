b1d0f11afa2c920d6811c848ec246d961fce059b
mgrankin <mv.grankin@gmail.com>
2019-09-13 15:59:30 +0300

    lr

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,9 +48,6 @@ PYTHONPATH=src ./train.py --model_name 117M --dataset ../data/total.npz \
 conda activate gpt2
 export CUDA_VISIBLE_DEVICES=2
 PYTHONPATH=src ./train.py --model_name 345M --dataset ../data/total.npz \
-    --run_name=poet2_345M --batch_size 3 --save_every 10000 --sample_every 1000 --learning_rate=2e-4
-
-PYTHONPATH=src ./train.py --model_name 345M --dataset ../data/total.npz \
     --run_name=poet_345M --batch_size 3 --save_every 10000 --sample_every 1000 --learning_rate=2e-5
 # 2.6
 
@@ -60,12 +57,9 @@ PYTHONPATH=src ./train.py --model_name 345M --dataset ../data/total.npz \
 # 774M
 conda activate gpt2
 export CUDA_VISIBLE_DEVICES=3
-#PYTHONPATH=src ./train.py --model_name 774M --dataset ../data/total.npz \
-#    --batch_size 1 --save_every 10000 --sample_every 1000 --learning_rate=1e-3 --optimizer=sgd
-# 3.0
-
 PYTHONPATH=src ./train.py --model_name 774M --dataset ../data/total.npz \
-    --run_name=poet_774M --batch_size 1 --save_every 10000 --sample_every 1000 --learning_rate=1e-2 --optimizer=sgd
+    --run_name=poet_774M --batch_size 1 --save_every 10000 --sample_every 1000 --learning_rate=1e-3 --optimizer=sgd
+# 3.0
 
 # t.me/NeuroPoetBot
 
